2b50bbb213fbec471dac05975cefe0ff8f6ffe5a
Rinat Ibragimov <rinatibragimov@rinats-mbp.attlocal.net>
2014-10-28 00:40:20 -0500

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Rinat Ibragimov on 10/28/14.
+//  Copyright (c) 2014 Rinat Ibragimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by Rinat Ibragimov on 10/28/14.
+//  Copyright (c) 2014 Rinat Ibragimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
