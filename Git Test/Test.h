b424d2a7d3ec186ce52ec797b7a942fbb121b1c4
Rinat Ibragimov <rinatibragimov@rinats-mbp.attlocal.net>
2014-10-28 01:05:43 -0500

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Rinat Ibragimov on 10/28/14.
+//  Copyright (c) 2014 Rinat Ibragimov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Rinat Ibragimov on 10/28/14.
+//  Copyright (c) 2014 Rinat Ibragimov. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
